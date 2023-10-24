29fdfd25e0dc45fd51fd5c31220020d7c3940f55
riichardgabriel <141831980+riichardgabriel@users.noreply.github.com>
2023-10-24 11:39:26 -0300

    Aulas de desenvolvimento em C#

---
+++ Olá Mundo/Olá Mundo/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Olá_Mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txbNumero2 = new System.Windows.Forms.TextBox();
+            this.txbNumero1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbresultado = new System.Windows.Forms.Label();
+            this.bntcalcular = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(15, 75);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(341, 71);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txbNumero2
+            // 
+            this.txbNumero2.Location = new System.Drawing.Point(162, 35);
+            this.txbNumero2.Name = "txbNumero2";
+            this.txbNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txbNumero2.TabIndex = 1;
+            // 
+            // txbNumero1
+            // 
+            this.txbNumero1.Location = new System.Drawing.Point(15, 35);
+            this.txbNumero1.Name = "txbNumero1";
+            this.txbNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txbNumero1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Numero 2";
+            // 
+            // lbresultado
+            // 
+            this.lbresultado.AutoSize = true;
+            this.lbresultado.Location = new System.Drawing.Point(301, 42);
+            this.lbresultado.Name = "lbresultado";
+            this.lbresultado.Size = new System.Drawing.Size(55, 13);
+            this.lbresultado.TabIndex = 5;
+            this.lbresultado.Text = "Resultado";
+            this.lbresultado.Click += new System.EventHandler(this.lbresultado_Click);
+            // 
+            // bntcalcular
+            // 
+            this.bntcalcular.Location = new System.Drawing.Point(15, 167);
+            this.bntcalcular.Name = "bntcalcular";
+            this.bntcalcular.Size = new System.Drawing.Size(341, 85);
+            this.bntcalcular.TabIndex = 9;
+            this.bntcalcular.Text = "subtração";
+            this.bntcalcular.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(297, 71);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "multiplicação";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(412, 167);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(297, 85);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "divisão";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bntcalcular);
+            this.Controls.Add(this.lbresultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbNumero1);
+            this.Controls.Add(this.txbNumero2);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txbNumero2;
+        private System.Windows.Forms.TextBox txbNumero1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbresultado;
+        private System.Windows.Forms.Button bntcalcular;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Olá Mundo/Olá Mundo/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Olá_Mundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            string numero1 = txbNumero1.Text;
+            string numero2 = txbNumero2.Text;
+
+            float total = float.Parse (numero1) / float.Parse (numero2);
+
+
+
+            lbresultado.Text = total.ToString();
+        }
+
+        private void lbresultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
